cc6667e9422db2ae3397d324aca71d9c6680da4f
Xander Dumaine <xander.dumaine@inin.com>
2019-03-04 15:40:12 -0500

    Update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  target: 'node',
-  entry: './src/StatsGatherer.js',
-  devtool: 'source-map',
-  mode: process.env.MINIMIZE ? 'production' : 'development',
-  optimization: {
-    minimize: !!process.env.MINIMIZE
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: process.env.MINIMIZE ? 'webrtc-stats-gatherer.min.js' : 'webrtc-stats-gatherer.js',
-    library: 'webrtc-troubleshooter',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env']
+module.exports = (env) => {
+  const minimize = env && env.production;
+  const node = env && env.node;
+  const file = minimize ? 'webrtc-stats-gatherer.min' : 'webrtc-stats-gatherer';
+  const extension = node ? '.cjs' : '.js';
+  const filename = file + extension;
+
+  return {
+    target: node ? 'node' : 'web',
+    entry: './src/StatsGatherer.js',
+    devtool: 'source-map',
+    mode: process.env.MINIMIZE ? 'production' : 'development',
+    optimization: {
+      minimize
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename,
+      library: 'webrtc-troubleshooter',
+      libraryTarget: 'umd'
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'global.GENTLY': false })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env']
+          }
         }
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
